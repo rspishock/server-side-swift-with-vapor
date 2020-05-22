0d95cfa8daaed6094329e7360c6c0706c3c924fe
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-05-22 14:40:20 -0400

    Initial commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,6 +11,15 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    router.get("hello", "vapor") { req in
+        return "Hello Vapor!"
+    }
+    
+    router.get("hello", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        return "Hello \(name)!"
+    }
 
     // Example of configuring a controller
     let todoController = TodoController()
